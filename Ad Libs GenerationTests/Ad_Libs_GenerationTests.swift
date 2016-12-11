e806d29929abc51cb88d354dc7750f55f7964165
Shane Doyle <shanebryandoyle@gmail.com>
2016-12-11 17:32:56 +0000

    Initial Commit

---
+++ Ad Libs GenerationTests/Ad_Libs_GenerationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ad_Libs_GenerationTests.swift
+//  Ad Libs GenerationTests
+//
+//  Created by Shane Doyle on 11/12/2016.
+//  Copyright © 2016 Shane Doyle. All rights reserved.
+//
+
+import XCTest
+@testable import Ad_Libs_Generation
+
+class Ad_Libs_GenerationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
