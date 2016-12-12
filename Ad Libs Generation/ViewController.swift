75e44196cd95839c92661ce0fab2957df7bac6e3
Shane Doyle <shanebryandoyle@gmail.com>
2016-12-12 21:50:40 +0000

    Initial commit

--- Ad Libs Generation/ViewController.swift
+++ Ad Libs Generation/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var textFields: [UITextField]!
 
+    @IBOutlet weak var pastTenseVerbTextField: UITextField!
+    @IBOutlet weak var singularNounTextField: UITextField!
+    @IBOutlet weak var pluralNounTextField: UITextField!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var locationSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var createLibsPressed: UIButton!
+    @IBOutlet weak var finishSentenceTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textFields = [pastTenseVerbTextField, singularNounTextField, pluralNounTextField, phraseTextField]
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        var location = ""
+        
+        location = "\(locationSegmentedControl.titleForSegment(at: locationSegmentedControl.selectedSegmentIndex)!)"
+        
+        finishSentenceTextView.text = "One evening at the \(location) a \(singularNounTextField.text!) \(pastTenseVerbTextField.text!) \(sliderLabel.text!) \(pluralNounTextField.text!) and said, \(phraseTextField.text!)"
+        finishSentenceTextView.backgroundColor = UIColor.yellow
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        sliderLabel.text = "\(Int(numberSlider.value))"
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let arrayOfTextFields = textFields as NSArray
+        let arrayIndex = arrayOfTextFields.index(of: textField)
+        
+        print(arrayIndex)
+        
+        if arrayIndex < arrayOfTextFields.count - 1 {
+            let newTextField = arrayOfTextFields[arrayIndex + 1] as! UITextField; newTextField.becomeFirstResponder()
+            let doneTextField = arrayOfTextFields[arrayIndex] as! UITextField; doneTextField.backgroundColor = UIColor.yellow
+        } else {
+            textField.resignFirstResponder()
+            textField.backgroundColor = UIColor.yellow
+        }
+        
+        return true
+    }
 }
 
